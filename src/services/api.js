a59e651a14fbf74942ab42b59b534e52e00e4de1
S.Rajesh <rajesh978812@gmail.com>
2026-02-01 11:46:53 +0530

    Updated API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://smartphone-backend-rgjm.onrender.com/api",
 });
 
 // Token Logic
